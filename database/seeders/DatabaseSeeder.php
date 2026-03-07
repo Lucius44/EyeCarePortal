e3f5b929d49977682005c949391f33a2066830d7
Lucius <jinclarence26@gmail.com>
2026-03-08 03:37:01 +0800

    Change seeder email admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             'middle_name'    => null,
             'last_name'      => 'Admin',
             'suffix'         => null,
-            'email'          => 'admin@eyecareportal.com',
+            'email'          => 'admineyecare@eyecareportal.com',
             'password'       => 'Password123', // Automatically hashed by User model casts
             'phone_number'   => '09123456789',
             'birthday'       => '1990-01-01',
